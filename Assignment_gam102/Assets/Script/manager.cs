024ceb8cc1171252682aa5206b480698d9dc0ae7
VinhTienDo <127204856+VinhTienDo@users.noreply.github.com>
2024-03-09 16:05:29 +0700

    trừ máu

--- Assignment_gam102/Assets/Script/manager.cs
+++ Assignment_gam102/Assets/Script/manager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class manager : MonoBehaviour
 {
+    public Slider thanhmau;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            thanhmau.value--;
+        }
     }
 }
